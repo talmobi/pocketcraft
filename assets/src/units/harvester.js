af9f37f7aaf37a010a14fb5fc3eafcf1ce706232
Hannyajin <talmo.christian@gmail.com>
2014-05-21 03:45:56 +0300

    masked assets

---
+++ assets/src/units/harvester.js
@@ -0,0 +1,13 @@
+/**
+  * Harvester
+  *
+  * Description: TODO
+  */
+
+function Harvester(x,y,team,color) {
+  var obj = {};
+
+
+
+  return obj;
+}
--- pocketcraft.js
+++ pocketcraft.js
@@ -12,7 +12,10 @@ var opts = {
 var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.WEBGL, container_id || 'phaser-container', opts);
 
 function preload() {
-  game.load.spritesheet('sheet', 'assets/sheet.png', 16, 16);
+  game.stage.backgroundColor = 0x222222;
+  game.stage.backgroundColor = 0xEECCEE;
+
+  game.load.spritesheet('sheet', 'assets/gfx/sheet.png', 16, 16);
 }
 
 var sprite;
@@ -30,10 +33,21 @@ function create() {
   sprite = game.add.sprite(game.world.centerX + 20, game.world.centerY, 'sheet');
   sprite.anchor.setTo(.5, 1);
 
-  sprite.animations.add('walk', [2, 3], 2, true);
+  sprite.animations.add('walk', [4, 5], 2, true);
   sprite.animations.play('walk');
 
   sprite.tint = 0xBBFFBB;
+
+  for (var i = 0; i < 4; i++) {
+    sprite = game.add.sprite(game.world.centerX + 40 + 20 * i, game.world.centerY + 40, 'sheet');
+    sprite.anchor.setTo(.5, 1);
+    sprite.animations.add('walk', [2 + i * 2, 3 + i * 2], 2, true);
+    sprite.animations.play('walk');
+  }
+
+
+  var harvester = new Harvester(game.world.centerX + 10, game.world.centerY - 30, 0, 0xFFBBBB);
+  game.add.existing(harvester);
 }
 
 function render() {
@@ -41,3 +55,22 @@ function render() {
   //game.debug.spriteBounds(sprite);
 
 }
+
+
+/**
+  * Harvester Unit
+  */
+function Harvester(x,y,team,hexColor) {
+  // call the sprite constructor
+  Phaser.Sprite.call(this, game, x, y, 'sheet');
+
+  this.anchor.setTo(.5, 1);
+  this.animations.add('walk', [0, 1], 2, true);
+  this.animations.play('walk');
+
+  this.tint = hexColor || 0xFFFFFF;
+  this.team = team || 9;
+};
+
+Harvester.prototype = Object.create(Phaser.Sprite.prototype);
+Harvester.prototype.constructor = Harvester;
